18bd6ae94a0ba829d9a9724371c774e2593ea4c8
MinexAutomation <minex.automation@gmail.com>
2019-09-10 14:42:53 -0700

    The default extension method to AwsEc2ServerSecrets for getting an SftpClientWrapper was updated to provide a connected client. A method to provide an un-connected client was provided if desired.

--- source/R5T.AWS.EC2/Code/Extensions/AwsEc2ServerSecretsExtensions.cs
+++ source/R5T.AWS.EC2/Code/Extensions/AwsEc2ServerSecretsExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using Renci.SshNet;
+
 using R5T.AWS.EC2.Configuration;
 using R5T.NetStandard.SSH;
 
@@ -8,14 +10,37 @@ namespace R5T.AWS.EC2
 {
     public static class AwsEc2ServerSecretsExtensions
     {
-        public static SftpClientWrapper GetSftpClientWrapper(this AwsEc2ServerSecrets awsEc2ServerSecrets)
+        public static ConnectionInfo GetConnectionInfo(this AwsEc2ServerSecrets awsEc2ServerSecrets)
         {
-            var client = new SftpClientWrapper(
+            var connectionInfo = Utilities.GetConnectionInfo(
                 awsEc2ServerSecrets.Host,
                 awsEc2ServerSecrets.UserID,
                 awsEc2ServerSecrets.Password,
                 awsEc2ServerSecrets.PrivateKeyFilePath);
-            return client;
+            return connectionInfo;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="SftpClientWrapper"/> for which the Connect() method has NOT been called.
+        /// </summary>
+        public static SftpClientWrapper GetUnconnectedSftpClientWrapper(this AwsEc2ServerSecrets awsEc2ServerSecrets)
+        {
+            var connectionInfo = awsEc2ServerSecrets.GetConnectionInfo();
+
+            var clientWrapper = new SftpClientWrapper(connectionInfo);
+            return clientWrapper;
+        }
+
+        /// <summary>
+        /// Gets a connected <see cref="SftpClientWrapper"/>.
+        /// </summary>
+        public static SftpClientWrapper GetSftpClientWrapper(this AwsEc2ServerSecrets awsEc2ServerSecrets)
+        {
+            var clientWrapper = awsEc2ServerSecrets.GetUnconnectedSftpClientWrapper();
+
+            clientWrapper.SftpClient.Connect();
+
+            return clientWrapper;
         }
     }
 }
